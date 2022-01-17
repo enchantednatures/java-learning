b1ee97d1ca7e0522273ec5da0d543d1d1d67b771
Hunter Casten <41604962+enchantednatures@users.noreply.github.com>
2022-01-16 19:58:58 -0700

    added a doer of thing to do things.

---
+++ src/com/goobylicious/DoerOfThings.java
@@ -0,0 +1,13 @@
+package com.goobylicious;
+
+public class DoerOfThings {
+    public String name;
+
+    public DoerOfThings(String name) {
+        this.name = name;
+    }
+
+    public void doThings() {
+        System.out.println("I dun be doin' a thing Goobs");
+    }
+}
--- src/com/goobylicious/Program.java
+++ src/com/goobylicious/Program.java
@@ -5,8 +5,10 @@ public class Program {
 
     public static void main(String[] args) {
         ThingDoer myThingDoer = new ThingDoer("Thing Doer");
+        DoerOfThings myDoerOfThings = new DoerOfThings("Doer of Things");
 
         myThingDoer.doThings();
+        myDoerOfThings.doThings();
 
     }
 }
